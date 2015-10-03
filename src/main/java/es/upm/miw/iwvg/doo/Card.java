951d63052fe72fa944ba9fe38da27d0795b26430
Fernando Moro <fernando.moro@radmas.com>
2015-10-03 20:11:26 +0200

    Added first definition for Card

---
+++ src/main/java/es/upm/miw/iwvg/doo/Card.java
@@ -0,0 +1,13 @@
+package es.upm.miw.iwvg.doo;
+
+public class Card {
+    private Suit suit;
+    private int value;
+    
+    public Card(Suit suit, int value) {
+        super();
+        this.suit = suit;
+        this.value = value;
+    }
+
+}
