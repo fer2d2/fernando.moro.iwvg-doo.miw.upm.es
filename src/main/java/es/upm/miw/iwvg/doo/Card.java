58fd886243a2e9520627a1e1f8856bf4f082bcc8
Fernando Moro <fernando.moro@radmas.com>
2015-10-08 19:33:30 +0200

    Added uncovered property to Card

--- src/main/java/es/upm/miw/iwvg/doo/Card.java
+++ src/main/java/es/upm/miw/iwvg/doo/Card.java
@@ -3,10 +3,10 @@ package es.upm.miw.iwvg.doo;
 public class Card {
     private Suit suit;
     private int value;
+    private boolean uncovered = false;
     
     public Card(Suit suit, int value) {
         this.suit = suit;
         this.value = value;
     }
-
 }
